6bc43d167ce380ba6d4b3ebdfedc4750d4f051dc
Navani001 <navaneetha2006krishnan@gmail.com>
2025-07-18 18:03:53 +0530

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import { auth } from "@/utils";
 import { redirect } from "next/navigation";
-import { DashboardLayout } from "@/component";
-import DashboardContent from "@/component/dashboard/DashboardContent";
 
 export default async function Home() {
   const data: any = await auth();
@@ -12,8 +10,6 @@ export default async function Home() {
   }
 
   return (
-    <DashboardLayout user={data.user}>
-      <DashboardContent />
-    </DashboardLayout>
+   <div>hi</div>
   );
 }
